66a91f0bd06082ba70f721b7801f3eec848302d5
manhphongdev <tranmanhphong2002@gmail.com>
2026-06-02 20:38:56 +0700

    Initial commit

---
+++ src/test/java/com/muasamcong/MuasamcongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muasamcong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuasamcongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
